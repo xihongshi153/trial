e53eb874249d07ddba401c9be19a7f69a431f49f
谢佳君 <1533576984@qq.com>
2021-01-29 19:42:46 +0800

    对于定义，转换字符数组和字符串不够熟练。

--- week1/src/task_3.java
+++ week1/src/task_3.java
@@ -1,12 +1,22 @@
+
+
 import java.util.Scanner;
 
 public class task_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
+        String str=in.nextLine();
+        char[] ch=str.toCharArray();
+        ch=fun(ch);
+        for(int i=0;i<ch.length;i++)
+        {System.out.print(ch[i]);}
+
     }
-    public static String fun(String str){
-
-
+    public static char[] fun(char[] a1)
+    {  char[] a2=new char[a1.length];
+       for(int i =a1.length-1,j=0 ;i>=0;i--,j++){
+           a2[j]=a1[i];
+       }
+           return a2;
     }
 }
