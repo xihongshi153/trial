ed1abe276da731d183761fdd4c13cacb4d8f4e8f
谢佳君 <1533576984@qq.com>
2021-02-06 16:15:33 +0800

    这个不是很明白，怎么隔开找到的数字呢？

--- week2/src/task_2.java
+++ week2/src/task_2.java
@@ -1,15 +1,2 @@
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
-
 public class task_2 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("请输入字符str=");
-        String str=in.nextLine();
-        char[] cha=str.toCharArray();
-        for (int i = 0; i < cha.length-1; i++) {
-            if(cha[i]<'9'&&cha[i]>'0')
-                System.out.println(cha[i]);
-        }
-    }
 }
