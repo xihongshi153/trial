977df91c8112009a9ab6b09f73fc25727fde2cfe
西红柿 <1533576984@qq.com>
2021-03-13 17:05:36 +0800

    NULL

--- week5/src/java_task_2.java
+++ week5/src/java_task_2.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class java_task_2 {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(climbStairs(n));
+    }
+
+    public static int climbStairs(int n) {
+        if(n==1) return 1;
+        int a = 1;
+        int b = 2;
+        int c;
+        for(int i=3;i<=n ;++i){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 }
